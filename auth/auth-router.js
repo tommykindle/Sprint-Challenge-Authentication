4142c128c282d07399cd0e00cead28a5d0b844ec
Tommy <tommykindle83@gmail.com>
2019-09-20 11:01:32 -0700

    sprint finished MVP reached

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,57 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const Users = require('./auth-model')
+
+const secrets = require('../database/config/secrets')
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 16)
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error);
+    })
 });
 
 router.post('/login', (req, res) => {
-  // implement login
-});
+  let { username, password } = req.body
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    });
+})
+
+
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
 
 module.exports = router;
